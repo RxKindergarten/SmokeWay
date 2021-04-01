fa0d32141c282448f3606ebaae89be2dfe8ea987
qodhrkawk <gabriel1120@naver.com>
2021-04-01 21:42:36 +0900

    create : 지도 뷰 만드는중 feature/#2

--- SmokeWay/SmokeWay/Apps/Scene/MainScene/MainVC.swift
+++ SmokeWay/SmokeWay/Apps/Scene/MainScene/MainVC.swift
@@ -8,14 +8,48 @@
 import UIKit
 import Firebase
 import NMapsMap
+import CoreLocation
 
-class MainVC: UIViewController {
+class MainVC: UIViewController,CLLocationManagerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager.startUpdatingLocation()
+        
+        
+        
+        
+        let marker = NMFMarker()
         // Do any additional setup after loading the view.
         let mapView = NMFMapView(frame: view.frame)
         view.addSubview(mapView)
+        mapView.positionMode = .direction
+//        mapView.showLocationButton = true
+        let locationOverlay = mapView.locationOverlay
+        locationOverlay.hidden = false
+        locationOverlay.iconWidth = CGFloat(NMF_LOCATION_OVERLAY_SIZE_AUTO)
+        locationOverlay.iconHeight = CGFloat(NMF_LOCATION_OVERLAY_SIZE_AUTO)
+        let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: 37.5666102, lng: 126.9783881))
+        
+        marker.position = NMGLatLng(lat: 37.5670135, lng: 126.9783740)
+        marker.mapView = mapView
+        
+        let infoWindow = NMFInfoWindow()
+        infoWindow.open(with: marker)
+        
+        cameraUpdate.animation = .fly
+        cameraUpdate.animationDuration = 3
+        mapView.moveCamera(cameraUpdate)
+        
+        
+        
     }
     
 
--- SmokeWay/SmokeWay/Apps/Scene/MainScene/MainViewModel.swift
+++ SmokeWay/SmokeWay/Apps/Scene/MainScene/MainViewModel.swift
@@ -13,32 +13,32 @@ protocol MainViewModelType  {
     associatedtype Input
     associatedtype Output
     
-    func transform(input: Input) -> Output
+//    func transform(input: Input) -> Output
 }
 
-//class MainViewModel: MainViewModelType {
-//    
-//    struct Input {
-//        let ready: Driver<Bool>
-//        let currentPoint: Driver<MapPoint>
-//        let selectedPoint: Driver<MapPoint>
-//        let expansion: Driver<Expansion>
-//    }
-//    
-//    struct Output {
-//        let loading: Driver<Bool>
-//        let surroundInfos: Driver<[SmokingPlace]>
-//        let sortedInfos: Driver<[SmokingPlace]>
-//        let exapansion: Driver<Expansion>
-//        let detailInfo: Driver<SmokingPlace>
-//        
-//    }
-//    
-//    
-//    struct Dependencies {
-//        
-//    }
-//    
+class MainViewModel: MainViewModelType {
+    
+    struct Input {
+        let ready: Driver<Bool>
+        let currentPoint: Driver<MapPoint>
+        let selectedPoint: Driver<MapPoint>
+        let expansion: Driver<Expansion>
+    }
+    
+    struct Output {
+        let loading: Driver<Bool>
+        let surroundInfos: Driver<[SmokingPlace]>
+        let sortedInfos: Driver<[SmokingPlace]>
+        let exapansion: Driver<Expansion>
+        let detailInfo: Driver<SmokingPlace>
+        
+    }
+    
+    
+    struct Dependencies {
+        
+    }
+    
 //    func transform(input: Input) -> Output {
 //        
 //        let loading = input.ready
@@ -56,6 +56,6 @@ protocol MainViewModelType  {
 //    
 //    
 //    let smokingPlaces: [SmokingPlace]
-//}
+}
 
 
